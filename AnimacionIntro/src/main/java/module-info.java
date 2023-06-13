f81f015c86ac27bfeb47146cae0b6c012f659dbc
AlexisJ16 <114438880+AlexisJ16@users.noreply.github.com>
2023-06-12 19:22:23 -0500

    Delete AnimacionIntro directory

--- AnimacionIntro/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.animacionintro {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.animacionintro to javafx.fxml;
-    exports com.example.animacionintro;
-
-}
